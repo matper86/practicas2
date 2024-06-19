0d500cb25ee9ce49f7435e3471fc9cf598f095b4
“matiper.developer@gmail.com” <matiper.developer@gmail.com>
2024-06-18 21:35:40 -0300

    Estructura Login Creada

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production:false,
+    apiUrl:'http://fakestoreapi.com/'
+};
+
+// export const environment = {
+//     production: false,
+//     apiUrl: 'http://fakestoreapi.com/',
+//     enableDebug: true,
+//     featureFlag: true,
+//   };
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production:true,
+  apiUrl:'http://fakestoreapi.com/'
+};
+
+// export const environment = {
+//     production: true,
+//     apiUrl: 'http://fakestoreapi.com/',
+//     featureFlag: false,
+//   };
